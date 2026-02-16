9c01cf6179223abe21e8104ea50133b416bc28d6
Alexandre <als.stumpf@gmail.com>
2026-02-16 15:22:16 -0300

    corrigindo bugs e atribuindo atraso da imagem do demogorgão (imagem paralax)

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,5 @@ gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText);
 //criando um scroll suave
 ScrollSmoother.create({
   smooth: 1.5, // quanto tempo (em segundos) leva para "alcançar" a posição de rolagem nativa.
+  effects: true // procura atributos de velocidade de dados e atraso de dados em elementos
 });
