1cc1c6cc7ebf49a038968a244dc6e2cc5cec7a8c
Alexandre <als.stumpf@gmail.com>
2026-02-21 21:39:18 -0300

    criando animações da classe heroes

--- assets/script.js
+++ assets/script.js
@@ -6,3 +6,9 @@ ScrollSmoother.create({
   smooth: 1.5, // quanto tempo (em segundos) leva para "alcançar" a posição de rolagem nativa.
   effects: true // procura atributos de velocidade de dados e atraso de dados em elementos
 });
+
+//animações da classe heroes
+gsap.from(".heroes", {
+  opacity: 0,
+  duration: 2.75
+});
